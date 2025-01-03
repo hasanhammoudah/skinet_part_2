4bb3855d2030efaaf69987878830df2106215f3f
HasanHammoudah <hasanHammoudah>
2025-01-03 21:03:28 +0300

    first commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
