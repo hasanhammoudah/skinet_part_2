51fa88dca722ac32e17b04b365df0123f90a8e55
hasanhammoudah <hassun.h98@gmail.com>
2025-01-24 22:58:23 +0300

    feat: Add cart functionalities and summary component
    - Created SummaryComponent for displaying order summary.
    - Implemented CartService for managing cart operations.
    - Added CartItemComponent to handle individual cart items.
    - Developed ProductItems component for listing products.
    - Created init.service.ts to handle app initialization logic.
    - Updated several components and made edits to related files.

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36005858c7ebe3bcf5eda03f154e7a636a9acde4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9be3247237630dce732651d07f880149ec68a9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
